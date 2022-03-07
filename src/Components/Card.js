5863af097759daef60cfc0d7684ceee7b8905162
Vlad Dragoi <vladdragoi1@gmail.com>
2022-03-07 22:51:54 +0200

    refactoring

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,31 +3,22 @@ import Form from "./Form";
 import Diagram from "./Diagram";
 
 const Card = () => {
-	const [mass, setMass] = useState("");
-	const [wingArea, setWingArea] = useState("");
-	const [loadFactor, setLoadFactor] = useState("");
-	const [CLMax, setCLMax] = useState("");
+	const [values, setValues] = useState({
+		mass: "",
+		wingArea: "",
+		loadFactor: "",
+		CLMax: "",
+	});
+	console.log(values);
 
-	const submitValues = (mass, wingArea, loadFactor, CLMax) => {
-		setMass(mass);
-		setWingArea(wingArea);
-		setLoadFactor(loadFactor);
-		setCLMax(CLMax);
+	const submitValues = (values) => {
+		setValues(values);
 	};
 
 	return (
 		<div className="flex flex-col justify-center items-center my-8 px-8 py-6 w-10/12 max-w-sm rounded-2xl text-base font-sans bg-card1 lg:flex-row lg:my-0 lg:w-auto lg:max-w-max lg:justify-around">
 			<Form submitValues={submitValues} />
-			{mass > 0 ? (
-				<Diagram
-					mass={mass}
-					wingArea={wingArea}
-					loadFactor={loadFactor}
-					CLMax={CLMax}
-				/>
-			) : (
-				""
-			)}
+			{values.mass > 0 && <Diagram {...values} />}
 		</div>
 	);
 };
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,33 +1,53 @@
 import { useState } from "react";
 
 const Form = ({ submitValues }) => {
-	const [mass, setMass] = useState("");
-	const [wingArea, setWingArea] = useState("");
-	const [loadFactor, setLoadFactor] = useState("");
-	const [CLMax, setCLMax] = useState("");
+	const [values, setValues] = useState({
+		mass: "",
+		wingArea: "",
+		loadFactor: "",
+		CLMax: "",
+	});
 
 	const showDiagram = (e) => {
 		e.preventDefault();
-		submitValues(mass, wingArea, loadFactor, CLMax);
+		submitValues(values);
 	};
 
 	return (
 		<form className="w-full max-w-xs text-white" onSubmit={showDiagram}>
 			<AircraftType />
-			<Input label="Mass" unit="kg" value={mass} setValue={setMass} />
+			<Input
+				label="Mass"
+				unit="kg"
+				value={values.mass}
+				onChange={(e) => {
+					setValues({ ...values, mass: e.target.value });
+				}}
+			/>
 			<Input
 				label="Wing Area"
 				unit="m^2"
-				value={wingArea}
-				setValue={setWingArea}
+				value={values.wingArea}
+				onChange={(e) => {
+					setValues({ ...values, wingArea: e.target.value });
+				}}
 			/>
 			<Input
 				label="Max. Load Factor"
 				unit="+"
-				value={loadFactor}
-				setValue={setLoadFactor}
+				value={values.loadFactor}
+				onChange={(e) => {
+					setValues({ ...values, loadFactor: e.target.value });
+				}}
 			/>
-			<Input label="C_L_max" unit="" value={CLMax} setValue={setCLMax} />
+			<Input
+				label="C_L_max"
+				unit=""
+				value={values.CLMax}
+				onChange={(e) => {
+					setValues({ ...values, CLMax: e.target.value });
+				}}
+			/>
 			{/* <Input label="Cruising Speed" unit="m/s" /> */}
 			<CalculateButton text="Calculate!" />
 		</form>
@@ -46,14 +66,14 @@ const AircraftType = () => {
 	);
 };
 
-const Input = ({ label, unit, value, setValue }) => {
+const Input = ({ label, unit, value, onChange }) => {
 	return (
 		<div className="flex items-center justify-between my-4">
 			<label className=" w-26">{label}:</label>
 			<input
 				type="text"
 				value={value}
-				onChange={(e) => setValue(e.target.value)}
+				onChange={onChange}
 				className=" w-6/12 max-w-xxs mx-3 pl-2 bg-transparent border-white border focus:outline-none"
 			/>
 			<span className="w-20 text-right">({unit})</span>
