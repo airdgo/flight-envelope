4fab46d8cd41d47eebfe1cc72d9e49bd2e2e21ac
Vlad Dragoi <vladdragoi1@gmail.com>
2022-05-07 09:34:58 +0300

    changed github icon opacity on hover

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import { GitHubIco } from "./Icons/GitHubIco";
 export const Footer = () => {
 	return (
-		<div className="absolute bottom-0 text-[#fbfbfb] w-full opacity-75 py-3 px-4 flex justify-end">
+		<div className="absolute bottom-0 w-full py-3 px-4 flex justify-end">
 			<a href="https://github.com/airdgo" target="_blank">
 				<GitHubIco />
 			</a>
