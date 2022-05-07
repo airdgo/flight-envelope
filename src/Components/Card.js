e809225048506027e4bb9c58182ee6368d493cad
Vlad Dragoi <vladdragoi1@gmail.com>
2022-05-07 09:26:33 +0300

    removed some padding on mobile view

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -10,7 +10,7 @@ const Card = () => {
 	const maxWidth = values ? "lg:max-w-5xl max-w-2xl" : "max-w-max";
 
 	const className =
-		"flex flex-col gap-10 justify-center shadow-md items-center my-10 py-8 px-6 md:px-10 w-10/12 rounded-2xl text-base font-sans bg-primary lg:flex-row lg:my-0 lg:w-full lg:justify-around " +
+		"flex flex-col gap-10 justify-center shadow-md items-center my-10 py-6 px-6 md:py-8 md:px-10 w-10/12 rounded-2xl text-base font-sans bg-primary lg:flex-row lg:my-0 lg:w-full lg:justify-around " +
 		maxWidth;
 
 	return (
